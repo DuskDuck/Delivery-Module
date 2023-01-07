db1bf619e3fd1614c6029da229380beeb3ae5442
DuskDuck <107291973+DuskDuck@users.noreply.github.com>
2023-01-07 17:55:24 +0700

    Add files via upload

--- src/main/java/com/hedspi/ltct/delivery/DeliveryApplication.java
+++ src/main/java/com/hedspi/ltct/delivery/DeliveryApplication.java
@@ -3,15 +3,26 @@ package com.hedspi.ltct.delivery;
 import com.hedspi.ltct.delivery.controller.ShippingController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication(scanBasePackageClasses = {ShippingController.class})
+import java.util.Arrays;
+
+@SpringBootApplication//(scanBasePackageClasses = {ShippingController.class})
 public class DeliveryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DeliveryApplication.class, args);
+        //SpringApplication.run(DeliveryApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(DeliveryApplication.class, args);
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
     }
     @Bean
     public WebMvcConfigurer corsConfigurer() {
