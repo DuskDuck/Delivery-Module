86952234c50c53caeb390ed2b45bcfb69c107406
DuskDuck <107291973+DuskDuck@users.noreply.github.com>
2023-01-07 17:47:23 +0700

    final check

--- src/main/java/com/hedspi/ltct/delivery/DeliveryApplication.java
+++ src/main/java/com/hedspi/ltct/delivery/DeliveryApplication.java
@@ -1,12 +1,13 @@
 package com.hedspi.ltct.delivery;
 
+import com.hedspi.ltct.delivery.controller.ShippingController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = {ShippingController.class})
 public class DeliveryApplication {
 
     public static void main(String[] args) {
