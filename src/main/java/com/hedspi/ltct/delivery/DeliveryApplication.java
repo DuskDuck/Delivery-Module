6f0e40ee9548c254ba7a60a86f92591b8e0ae8aa
DuskDuck <107291973+DuskDuck@users.noreply.github.com>
2023-01-07 18:03:38 +0700

    Add files via upload

--- src/main/java/com/hedspi/ltct/delivery/DeliveryApplication.java
+++ src/main/java/com/hedspi/ltct/delivery/DeliveryApplication.java
@@ -10,27 +10,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 
-@SpringBootApplication//(scanBasePackageClasses = {ShippingController.class})
+@SpringBootApplication(scanBasePackages = {"com.hedspi.ltct.delivery"})//(scanBasePackageClasses = {ShippingController.class})
 public class DeliveryApplication {
 
     public static void main(String[] args) {
-        //SpringApplication.run(DeliveryApplication.class, args);
-        ApplicationContext ctx = SpringApplication.run(DeliveryApplication.class, args);
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
-    }
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/*").allowedOrigins("http://localhost:4200");
-            }
-        };
+        SpringApplication.run(DeliveryApplication.class, args);
     }
 }
