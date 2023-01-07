63bcb75347c02ef99884690055251f2fb1828867
DuskDuck <107291973+DuskDuck@users.noreply.github.com>
2023-01-07 16:23:26 +0700

    Update ShippingController.java

--- src/main/java/com/hedspi/ltct/delivery/controller/ShippingController.java
+++ src/main/java/com/hedspi/ltct/delivery/controller/ShippingController.java
@@ -12,7 +12,7 @@ import java.time.Instant;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "api")
+@RequestMapping(path = "/api")
 public class ShippingController {
 
     private final ShippingService shippingService;
